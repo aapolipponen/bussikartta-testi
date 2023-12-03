23f243df221cc2877b1fa7afe04bbc5b83a507c8
aapolipponen <aapo.lipponen@gmail.com>
2023-12-03 13:27:05 +0200

    Improvements

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([60.45, 22.25], 13); // Adjust the center and zoom level to your preference
+var map = L.map('map').setView([60.45, 22.25], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap contributors'
@@ -6,31 +6,43 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var busMarkers = [];
 
+// Function to update bus locations
 function updateBusLocations() {
     axios.get('https://data.foli.fi/siri/vm')
-        .then(function (response) {
-            // Clear existing markers
-            busMarkers.forEach(marker => map.removeLayer(marker));
-            busMarkers = [];
-
-            // Check if the response contains the expected data
+        .then(response => {
+            clearMarkers();
             if (response.data && response.data.result && response.data.result.vehicles) {
-                var vehicles = response.data.result.vehicles;
-
-                // Iterate through the vehicles and create markers
-                for (var id in vehicles) {
-                    var bus = vehicles[id];
-                    var marker = L.marker([bus.latitude, bus.longitude]).addTo(map);
-                    busMarkers.push(marker);
-                }
+                addMarkers(response.data.result.vehicles);
             } else {
                 console.error('Unexpected response format:', response.data);
             }
         })
-        .catch(function (error) {
+        .catch(error => {
             console.error('Error fetching data:', error);
         });
 }
 
-setInterval(updateBusLocations, 3000); // Update every 3 seconds
+// Clear existing markers
+function clearMarkers() {
+    busMarkers.forEach(marker => map.removeLayer(marker));
+    busMarkers = [];
+}
+
+// Add markers to the map
+function addMarkers(vehicles) {
+    for (var id in vehicles) {
+        var bus = vehicles[id];
+        if (bus.latitude && bus.longitude && bus.publishedlinename) {
+            var marker = L.marker([bus.latitude, bus.longitude])
+                .bindPopup(`<b>${bus.publishedlinename}</b>`)
+                .addTo(map);
+            busMarkers.push(marker);
+        }
+    }
+}
+
+// Set interval slightly less than 3 seconds
+setInterval(updateBusLocations, 2800);
+
+// Initial call to start the process
 updateBusLocations();
