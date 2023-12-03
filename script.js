100d47b905cb40eb7608ad6e3ed42067d488039a
aapolipponen <aapo.lipponen@gmail.com>
2023-12-03 13:56:06 +0200

    Improvements, added stops

--- script.js
+++ script.js
@@ -1,8 +1,78 @@
-var map = L.map('map').setView([60.45, 22.25], 13);
+// Create a Leaflet map centered on Turku, Finland
+const map = L.map('map').setView([60.45065, 22.26296], 13);
+
+// Add a base map layer (You can replace 'mapbox/streets-v11' with your preferred tileset)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+// Custom icons for buses and bus stops
+const busIcon = L.icon({
+    iconUrl: 'https://fonts.gstatic.com/s/i/materialicons/directions_bus/v12/24px.svg',
+    iconSize: [24, 24],
+    iconAnchor: [12, 24],
+    popupAnchor: [0, -24]
+});
+
+const busStopIcon = L.icon({
+    iconUrl: 'https://fonts.gstatic.com/s/i/materialicons/departure_board/v11/24px.svg',
+    iconSize: [24, 24],
+    iconAnchor: [12, 24],
+    popupAnchor: [0, -24]
+});
+
+// Function to fetch loading points and cache them
+async function fetchAndCacheLoadingPoints() {
+    try {
+        const response = await fetch('http://data.foli.fi/geojson/poi/loading_points');
+        const data = await response.json();
+
+        // Store the loading points data in local storage
+        localStorage.setItem('loadingPoints', JSON.stringify(data));
+
+        // Create a GeoJSON layer with custom icons and add it to the map
+        L.geoJSON(data, {
+            pointToLayer: function (feature, latlng) {
+                if (feature.properties.category === "LOADING_POINT") {
+                    return L.marker(latlng, { icon: busStopIcon });
+                }
+            },
+            onEachFeature: function (feature, layer) {
+                if (feature.properties && feature.properties.name) {
+                    layer.bindPopup(feature.properties.name + "<br>" + feature.properties.address);
+                }
+            }
+        }).addTo(map);
+
+        // Fit the map to show all markers
+        map.fitBounds(L.geoJSON(data).getBounds());
+    } catch (error) {
+        console.error('Error fetching/loading loading points:', error);
+    }
+}
+
+// Fetch and cache loading points if not already cached
+if (!localStorage.getItem('loadingPoints')) {
+    fetchAndCacheLoadingPoints();
+} else {
+    // Load cached loading points data and create the GeoJSON layer
+    const cachedLoadingPoints = JSON.parse(localStorage.getItem('loadingPoints'));
+    L.geoJSON(cachedLoadingPoints, {
+        pointToLayer: function (feature, latlng) {
+            if (feature.properties.category === "LOADING_POINT") {
+                return L.marker(latlng, { icon: busStopIcon });
+            }
+        },
+        onEachFeature: function (feature, layer) {
+            if (feature.properties && feature.properties.name) {
+                layer.bindPopup(feature.properties.name + "<br>" + feature.properties.address);
+            }
+        }
+    }).addTo(map);
+
+    // Fit the map to show all markers
+    map.fitBounds(L.geoJSON(cachedLoadingPoints).getBounds());
+}
 
 var busMarkers = [];
 
@@ -41,8 +111,6 @@ function addMarkers(vehicles) {
     }
 }
 
-// Set interval slightly less than 3 seconds
-setInterval(updateBusLocations, 2800);
-
-// Initial call to start the process
+// Automatically fetch and display buses on the map
 updateBusLocations();
+setInterval(updateBusLocations, 2800); // Refresh bus locations every 2.8s
